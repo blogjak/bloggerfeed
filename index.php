8345669a6f35f17c6298a13a4efa8e35d678d8c9
bezimeniid <bezimeni.id@gmail.com>
2017-07-25 15:02:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-$blogID='xxxxxxxxxxx';
+ // Tutorial: http://blogjak.com/?A7WG
+// 482697935644832514 Coklat
+// 8328682700413450881 Danatunai
+// 8071207005090667471 Bloglaundry
+// 5156676174768913851 PPH
+// 4252636256666591002 Parfumlaundry
+
+
+$blogID='5156676174768913851';
 $requestURL="http://www.blogger.com/feeds/{$blogID}/posts/default";
 $xml=simplexml_load_file($requestURL);
 ?>
